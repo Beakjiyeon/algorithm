a63623ea0d29fb7d9346d6b254c5a4234eb65728
Beakjiyeon <bjy1530@naver.com>
2021-10-01 21:22:54 +0900

    최단경로 : 다익스트라 알고리즘 추가

---
+++ 최단 경로/다익스트라 알고리즘.py
@@ -0,0 +1,48 @@
+import heapq # 우선순위 큐
+import sys
+input = sys.stdin.readline 
+INF = int(1e9)
+
+n, m = map(int, input().split()) # 노드 개수, 간선 개수
+start = int(input()) # 시작 노드 번호
+
+# 그래프 생성
+graph = [[] for i in range(n + 1)]
+# 최단 거리 테이블 무한으로 초기화
+distance = [INF] * (n + 1) 
+
+# 모든 간선 정보 입력받기
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b,c)) # a번 노드에서 b번 노드로 가는 비용이 c. (노드, 거리)
+
+def dijkstra(start):
+    q = []
+    # 시작 노드 큐에 삽입
+    heapq.heappush(q, (0, start)) 
+
+    while q:
+        # 힙에서 최단거리 노드 정보 꺼내기
+        dist, now = heapq.heappop(q)
+        # 이미 방문했던 노드라면
+        if distance[now] < dist: 
+            continue
+        # 현재 노드와 연결된 다른 인접한 노드들을 확인
+        for i in graph[now]:
+            cost = dist + i[1] # 비용
+            # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우 갱신
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+dijkstra(start)
+
+# 모든 노드로 가기 위한 최단 거리
+for i in range(1, n +1):
+    if distance[i] ==INF :
+        print("INFINITY")
+    else:
+        print(distance[i])
+        
+
+
