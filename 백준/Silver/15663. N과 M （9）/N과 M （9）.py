39c2b41d29338853fdb073b914c3caea03808a18
Jiyeon Baek <bjy1530@naver.com>
2023-08-09 21:16:10 +0900

    [Silver II] Title: N과 M (9), Time: 100 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -0,0 +1,23 @@
+n, m = map(int, input().split())
+nums = sorted(list(map(int, input().split())))
+visited = [False] * n
+temp = []
+
+def dfs():
+    if len(temp) == m:
+        print(*temp)
+        return
+    remember_me = 0
+    for i in range(n):
+        if not visited[i] and remember_me != nums[i]:
+            visited[i] = True
+            temp.append(nums[i])
+            remember_me = nums[i]
+            dfs()
+            visited[i] = False
+            temp.pop()
+
+# https://honggom.tistory.com/111
+
+if __name__ == '__main__':
+    dfs()
