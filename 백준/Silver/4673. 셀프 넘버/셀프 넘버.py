ac7247335882db5e55a75ccbc18689b4ceeb9a55
Jiyeon Baek <bjy1530@naver.com>
2024-02-13 20:16:58 +0900

    [Silver V] Title: 셀프 넘버, Time: 580 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -1,10 +1,7 @@
-
-
-numbers =[]
+numbers = []
 for i in range(1, 10001):
     nums = list(map(int, list(str(i))))
-    number = i + sum(nums)
-    numbers.append(number)
+    numbers.append(i + sum(nums))
 
 for i in range(1, 10001):
     if i not in numbers:
