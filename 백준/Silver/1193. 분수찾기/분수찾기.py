9030c8a6953d0ae55e4b2a7f07fa20f7a4d93253
Jiyeon Baek <bjy1530@naver.com>
2024-03-06 23:28:51 +0900

    [Silver V] Title: 분수찾기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.py
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -1,17 +1,23 @@
-num = int(input())
-line = 1
+n = int(input())
 
-while num > line:
-    num -= line
-    line += 1
-    
-# 짝수일경우
-if line % 2 == 0:
-    a = num
-    b = line - num + 1
-# 홀수일경우
-elif line % 2 == 1:
-    a = line - num + 1
-    b = num
+row = 1
+max_count = 0
+while True:
+    max_count += row
 
-print(f'{a}/{b}')
+    if n <= max_count:
+        # print(f'{row}번째 ')
+        break
+    row += 1
+
+# 이전 줄 까지의 분수 개수
+cnt_by_prev_row = max_count - row
+
+# n 번 째 분수는 row 줄의 몇 번 째
+th_in_current_row = n - cnt_by_prev_row
+
+if row % 2 == 0:
+    result = f'{th_in_current_row}/{int(row) - int(th_in_current_row) + 1}'
+else:
+    result = f'{int(row) - int(th_in_current_row) + 1}/{th_in_current_row}'
+print(result)
