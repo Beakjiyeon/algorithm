307dfd8cc386d051dd39bf3102e47b20bd8090de
Jiyeon Baek <bjy1530@naver.com>
2024-03-06 21:42:42 +0900

    [Silver I] Title: 회의실 배정, Time: 4120 ms, Memory: 52684 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,12 +1,3 @@
-def greedy(meeting):
-    cnt = 0
-    start_t = -1
-    for time in meeting:
-        if time[0] >= start_t:
-            start_t = time[1]
-            cnt += 1
-    return cnt
-
 mCount = int(input())
 meeting = []
 for i in range(mCount):
@@ -14,4 +5,12 @@ for i in range(mCount):
     meeting.append((start, end))
 
 meeting = sorted(meeting, key=lambda time: (time[1], time[0]))
-print(greedy(meeting))
+
+cnt = 0
+start_t = -1
+for time in meeting:
+    if time[0] >= start_t:
+        start_t = time[1]
+        cnt += 1
+
+print(cnt)
